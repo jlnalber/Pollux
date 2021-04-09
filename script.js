99712ef73004b82b858a2e50bd3bd00f7c22fa2a
Julian <69533456+jlnalber@users.noreply.github.com>
2021-04-09 11:27:36 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function showPdfHelp(){
-    var pdfFrame = document.createElement("iframe");
-    pdfFrame.src = "Hilfe.html";
-    var content = document.getElementById("content");
-    content.innerHTML = "";
-    content.appendChild(pdfFrame);
-}
